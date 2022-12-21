28a562c986acd2984bf568470fa8434f29fd077e
Gift David <giftdavid@Gifts-MacBook-Air.local>
2022-12-21 17:07:30 +0100

    initial setup

---
+++ vertu-motors/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className={"container"}>App</div>
+    </div>
+  );
+}
+
+export default App;
