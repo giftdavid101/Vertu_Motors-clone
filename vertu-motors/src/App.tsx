494ea17d00274333762c33725edd3548ee03f840
Gift David <giftdavid@Gifts-MacBook-Air.local>
2022-12-22 16:55:56 +0100

    WIP

--- vertu-motors/src/App.tsx
+++ vertu-motors/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.scss";
+import Header from "./components/organisms/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div className={"container"}>App</div>
     </div>
   );
---
+++ vertu-motors/src/components/organisms/header.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import logo from "../../assets/img/logo.png";
+import { IoMdSearch } from "react-icons/io";
+import { GrLocation } from "react-icons/gr";
+import { RxPerson } from "react-icons/rx";
+import { FiPhone } from "react-icons/fi";
+import { BiInfoCircle } from "react-icons/bi";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Link } from "react-router-dom";
+import Input from "../atom/input";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <Link to={"/"}>Fleet</Link>
+        <Link to={"/"}>Careers</Link>
+        <Link to={"/"}>Investor</Link>
+        <Link to={"/"}>Contact Us</Link>
+        <Link to={"/"}>Newsroom</Link>
+        <div>
+          <FiPhone />
+          Call us on 0330 042 5132 (Sales open until 9pm)
+          <BiInfoCircle />
+        </div>
+      </div>
+      <div>
+        <div>
+          <Link to={"/"}>New cars</Link>
+          <Link to={"/"}>Used cars</Link>
+          <Link to={"/"}>Servicing</Link>
+          <Link to={"/"}>Vans</Link>
+          <Link to={"/"}>Electric Cars</Link>
+          <Link to={"/"}>Motability</Link>
+          <Link to={"/"}>Motorcycles</Link>
+          <MdKeyboardArrowDown />
+        </div>
+        <Input />
+      </div>
+      <div>
+        <div>
+          <AiOutlineMenu className={"menu"} />
+          <img alt={"logo"} src={logo} />
+        </div>
+        <div>
+          <IoMdSearch />
+          <GrLocation />
+          <RxPerson />
+          <FiPhone />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
