c2e4ac65b5dbd3cc5e4fea4e126b8cc13b897ab3
Gift David <giftdavid@Gifts-MacBook-Air.local>
2022-12-23 23:03:02 +0100

    feat: car logos

---
+++ vertu-motors/src/components/organisms/carlogos/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const CarLogos = () => {
+  return (
+    <div style={{ background: "gray" }}>
+      <img src={"https://www.vertumotors.com/custom/81464.svg"} alt={"audi"} />
+      <img src={"https://www.vertumotors.com/custom/93340.png"} alt={"BMW"} />
+      <img src={"https://www.vertumotors.com/custom/81032.svg"} alt={"Honda"} />
+      <img
+        src={"https://www.vertumotors.com/custom/81573.svg"}
+        alt={"Jaguar"}
+      />
+      <img src={"https://www.vertumotors.com/custom/81468.svg"} alt={"KIA"} />
+      <img
+        src={"https://www.vertumotors.com/custom/81572.svg"}
+        alt={"Land Rover"}
+      />
+      <img src={"https://www.vertumotors.com/custom/81465.svg"} alt={"Benz"} />
+      <img src={"https://www.vertumotors.com/custom/81589.svg"} alt={"Mini"} />
+      <img src={"https://www.vertumotors.com/custom/81466.svg"} alt={"Smart"} />
+      <img
+        src={"https://www.vertumotors.com/custom/81570.svg"}
+        alt={"Toyota"}
+      />
+      <img
+        src={"https://www.vertumotors.com/custom/81065.svg"}
+        alt={"Volkswagon"}
+      />
+    </div>
+  );
+};
+
+export default CarLogos;
