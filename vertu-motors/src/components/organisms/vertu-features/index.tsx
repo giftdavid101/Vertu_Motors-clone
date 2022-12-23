87adb3c37d925bf1fc8c23a28136dd1f6ccf3740
Gift David <giftdavid@Gifts-MacBook-Air.local>
2022-12-23 23:16:46 +0100

    feat: vertu motors feature

---
+++ vertu-motors/src/components/organisms/vertu-features/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { FaRegCheckCircle } from "react-icons/fa";
+
+const VertuMotorsFeatures = () => {
+  return (
+    <div>
+      <div>
+        <p>
+          If you're looking for new and used cars, vans or motorcycles from
+          Audi, Mercedes-Benz, Jaguar, Land Rover, smart, BMW, MINI, Honda,
+          Volkswagen, Kia and Toyota then you're in the right place. Your Vertu
+          Motors experience is guaranteed to be second-to-none from start to
+          finish.
+        </p>
+        <p>
+          {" "}
+          As part of one of the largest automotive retailer groups in the UK we
+          can offer both new and used cars, vans and motorcycles nationwide. But
+          the Vertu Motors experience doesn't end with handing over the keys -
+          our network of well-equipped service centres can offer everything you
+          need to keep moving, right up until it's time to choose your next
+          vehicle. Servicing, MOTs, vehicle repair, parts and more - all
+          delivered by manufacturer accredited technicians. Click here to find a
+          Vertu Motors dealership near you.
+        </p>{" "}
+        <p>
+          Here at Vertu Motors, we pride ourselves in delivering the very
+          highest standard of service every single day. Our Contact Centre is
+          available 7 days a week, meaning our friendly team of experts is
+          always here to help.
+        </p>
+      </div>
+      <div>
+        Excellent deals on New and Used Audi, Mercedes-Benz, Jaguar, Land Rover,
+        smart, BMW, MINI, Honda, Volkswagen, Kia and Toyota cars Comprehensive
+        and friendly Motability service Supplier of New and Used Mercedes-Benz,
+        Volkswagen and Toyota vans and commercial vehicles Supplier of New and
+        Used Honda and BMW Motorcycles Our technicians are trained to
+        manufacturer standards Our modern, clean workshops are equipped with the
+        latest tools and technology We're part of one of the largest automotive
+        retailer groups in the UK Customer support is available 7 days a week
+      </div>
+      <div>
+        <FaRegCheckCircle />
+        Excellent deals on New and Used Audi, Mercedes-Benz, Jaguar, Land Rover,
+        smart, BMW, MINI, Honda, Volkswagen, Kia and Toyota cars Comprehensive
+        and friendly Motability service Supplier of New and Used Mercedes-Benz,
+        Volkswagen and Toyota vans and commercial vehicles Supplier of New and
+        Used Honda and BMW Motorcycles Our technicians are trained to
+        manufacturer standards Our modern, clean workshops are equipped with the
+        latest tools and technology We're part of one of the largest automotive
+        retailer groups in the UK Customer support is available 7 days a week
+      </div>
+    </div>
+  );
+};
+
+export default VertuMotorsFeatures;
