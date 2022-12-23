e28a012c855f242c0ca4e23df264c855492902f0
Gift David <giftdavid@Gifts-MacBook-Air.local>
2022-12-23 22:19:03 +0100

    feat/special-offers card

--- vertu-motors/src/components/molecules/card/index.tsx
+++ vertu-motors/src/components/molecules/card/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { SlArrowRight } from "react-icons/sl";
 
-const Card: React.FC<{ imgSrc: string; subContent: any; imgAlt: string }> = ({
-  imgSrc,
-  subContent,
-  imgAlt,
-}) => {
+export type CardT = {
+  imgSrc: string;
+  subContent: any;
+  imgAlt: string;
+  title?: string;
+  name?: string;
+  price?: string;
+  monthlyPrice?: string;
+  moreDetails?: string;
+};
+const Card: React.FC<CardT> = ({ imgSrc, subContent, imgAlt }) => {
   return (
     <div>
       <div className={"card__image-container"}>
