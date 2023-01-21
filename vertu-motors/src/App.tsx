ff5156068cbc27900f7f31ba6ad81b7ab46f0cca
Gift <giftdavid101@gmail.com>
2023-01-21 21:25:32 +0100

    style fix

--- vertu-motors/src/App.tsx
+++ vertu-motors/src/App.tsx
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.scss";
 import Index from "./components/organisms/header";
 import Billboard from "./components/organisms/billboard";
 
 function App(props: any) {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>click me</button>
-      </div>
       <Index />
       <div className={"container"}>App</div>
       <Billboard />
